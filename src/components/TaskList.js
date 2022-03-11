2f2c321a4703dd2abcf17f5cdd10e745d88a110d
Betty Lam <[replace email]>
2022-03-10 16:48:47 -0800

    Screen tutorial

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
 import Task from "./Task";
 import { useDispatch, useSelector } from "react-redux";
